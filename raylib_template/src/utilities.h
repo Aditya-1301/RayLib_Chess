fab3c0d234f448072205c1dd9369fea30a4fda4b
Aditya Gupta <adigup@kth.se>
2023-12-19 18:08:18 +0000

    Utility functions are now in namespace Utilities_N

--- raylib_template/src/utilities.h
+++ raylib_template/src/utilities.h
@@ -7,76 +7,77 @@
 #include "ChessPiece.h"
 #include <vector>
 #include <iostream>
+using namespace ChessPiece_N;
 
-#define W_WIDTH 800 //500
-#define W_HEIGHT 800 //500
+namespace Utilities_N{
 
-#define tile_size 100
+    #define W_WIDTH 800
+    #define W_HEIGHT 800
 
-#ifndef CHESS_CPP_UTILITIES_H
-#define CHESS_CPP_UTILITIES_H
+    #define tile_size 100
 
-typedef struct piece{
-    ChessPiece * cp;
-    Rectangle * dr;
-} c_pieces;
+    typedef struct piece {
+        ChessPiece* cp;
+        Rectangle* dr;
+    } c_pieces;
 
-ChessPiece * selectedPiece = nullptr;
-std :: vector<c_pieces> pieces;
-int index;
-int turnCount = 0;
+    extern ChessPiece* selectedPiece;
+    extern std::vector<c_pieces> pieces;
+    extern int index;
+    extern int turnCount;
 
-Texture2D imageR1;
-Texture2D imageR2;
-Texture2D imageB1;
-Texture2D imageB2;
-Texture2D imageKn1;
-Texture2D imageKn2;
-Texture2D imageK;
-Texture2D imageQ;
-Texture2D imageP;
+    inline Texture2D imageR1;
+    inline Texture2D imageR2;
+    inline Texture2D imageB1;
+    inline Texture2D imageB2;
+    inline Texture2D imageKn1;
+    inline Texture2D imageKn2;
+    inline Texture2D imageK;
+    inline Texture2D imageQ;
+    inline Texture2D imageP;
 
-Texture2D image_r1;
-Texture2D image_r2;
-Texture2D image_b1;
-Texture2D image_b2;
-Texture2D image_kn1;
-Texture2D image_kn2;
-Texture2D imageK2;
-Texture2D imageQ2;
-Texture2D image_p;
+    inline Texture2D image_r1;
+    inline Texture2D image_r2;
+    inline Texture2D image_b1;
+    inline Texture2D image_b2;
+    inline Texture2D image_kn1;
+    inline Texture2D image_kn2;
+    inline Texture2D imageK2;
+    inline Texture2D imageQ2;
+    inline Texture2D image_p;
 
-Image rookWImage;
-Image bishopWImage;
-Image knightWImage;
-Image kingWImage;
-Image queenWImage;
-Image pawnWImage;
+    inline Image rookWImage;
+    inline Image bishopWImage;
+    inline Image knightWImage;
+    inline Image kingWImage;
+    inline Image queenWImage;
+    inline Image pawnWImage;
 
-Image rookBImage;
-Image bishopBImage;
-Image knightBImage;
-Image kingBImage;
-Image queenBImage;
-Image pawnBImage;
+    inline Image rookBImage;
+    inline Image bishopBImage;
+    inline Image knightBImage;
+    inline Image kingBImage;
+    inline Image queenBImage;
+    inline Image pawnBImage;
 
-Rectangle sourceRectKing;
-Rectangle sourceRectQueen;
-Rectangle sourceRectRook;
-Rectangle sourceRectBishop;
-Rectangle sourceRectKnight;
-Rectangle sourceRectPawn;
+    inline Rectangle sourceRectKing;
+    inline Rectangle sourceRectQueen;
+    inline Rectangle sourceRectRook;
+    inline Rectangle sourceRectBishop;
+    inline Rectangle sourceRectKnight;
+    inline Rectangle sourceRectPawn;
 
-void boardColorsSet(std::vector<std::vector<Color>>& boardColors);
-void RenderChessBoard();
-void InitTextures();
-void UnloadTextures();
-bool IsSameColor(std::vector<c_pieces> pieces, ChessPiece * selectedPiece, Position dest);
-int SetCapturedIndices(std::vector<c_pieces> pieces, Position dest);
-void CheckMoveValidityForSelectedPiece(std::vector<c_pieces>& pieces, Position dest);
-bool WhenButtonPressed(std::vector<c_pieces> pieces, int i, Vector2 offset, Vector2 mousePosition);
-void HandleChessPieceSelection(std::vector<c_pieces>& pieces, Vector2 offset);
-void RenderChessPieceImage(const std::vector<c_pieces>& pieces);
-std :: vector<c_pieces> returnPieces();
+    void boardColorsSet(std::vector<std::vector<Color>>& boardColors);
+    void RenderChessBoard();
+    void InitTextures();
+    void UnloadTextures();
+    bool IsSameColor(std::vector<c_pieces> pieces, ChessPiece* selectedPiece, Position dest);
+    int SetCapturedIndices(std::vector<c_pieces> pieces, Position dest);
+    void CheckMoveValidityForSelectedPiece(std::vector<c_pieces>& pieces, Position dest);
+    bool WhenButtonPressed(std::vector<c_pieces> pieces, int i, Vector2 offset, Vector2 mousePosition);
+    void HandleChessPieceSelection(std::vector<c_pieces>& pieces, Vector2 offset);
+    void RenderChessPieceImage(const std::vector<c_pieces>& pieces);
+    std::vector<c_pieces> returnPieces();
 
-#endif //CHESS_CPP_UTILITIES_H
+}
+
