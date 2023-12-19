46c92c1b0137f386536d188e4c4d3788af96a5a2
Aditya Gupta <adigup@kth.se>
2023-12-19 18:09:33 +0000

    ChessPiece functions are now in namespace ChessPiece_N
    Also added a utils_test function to test the utilities header.

--- raylib_template/src/chessPiece.cpp
+++ raylib_template/src/chessPiece.cpp
@@ -4,234 +4,249 @@
 
 #include <string>
 #include <vector>
-#include "chessPiece.h"
+#include "ChessPiece.h"
+#include "utilities.h"
+using namespace Utilities_N;
 
-std::string ChessPiece::getType(){
-    switch (this->type)
-    {
-        case KING:
-            return "King";
-        case QUEEN:
-            return "Queen";
-        case ROOK:
-            return "Rook";
-        case KNIGHT:
-            return "Knight";
-        case BISHOP:
-            return "Bishop";
-        case PAWN:
-            return "Pawn";
+void utils_test(){
+    std::vector<c_pieces> ps = returnPieces();
+    std::cout<<"\tUTILS TEST BEGIN\t"<<std::endl;
+    for (auto p : ps) {
+        std::cout<<p.cp->position.x<<" "<<p.cp->position.y<<std::endl;
     }
+    std::cout<<"\tUTILS TEST END\t"<<std::endl;
 }
 
-bool ChessPiece :: kingCheck(Position * position){
-    int x = (this->position).x;
-    int y = (this->position).y;
-    if( 0 > x || x > 8 || 0 > y || y > 8){
+namespace ChessPiece_N{
+
+    std::string ChessPiece::getType(){
+        utils_test();
+        switch (this->type)
+        {
+            case KING:
+                return "King";
+            case QUEEN:
+                return "Queen";
+            case ROOK:
+                return "Rook";
+            case KNIGHT:
+                return "Knight";
+            case BISHOP:
+                return "Bishop";
+            case PAWN:
+                return "Pawn";
+        }
+    }
+
+    bool ChessPiece :: kingCheck(Position * position){
+        int x = (this->position).x;
+        int y = (this->position).y;
+        if( 0 > x || x > 8 || 0 > y || y > 8){
+            return false;
+        }
+        Position surroundingPositions[] = {
+                Position(x-1, y-1),
+                Position(x-1, y),
+                Position(x-1, y+1),
+                Position(x, y-1),
+                Position(x, y+1),
+                Position(x+1, y-1),
+                Position(x+1, y),
+                Position(x+1, y+1)
+        };
+
+        for (int i = 0; i < sizeof(surroundingPositions) / sizeof(Position); i++) {
+            if (position->x == surroundingPositions[i].x && position->y == surroundingPositions[i].y){
+                return true;
+            }
+        }
         return false;
     }
-    Position surroundingPositions[] = {
-            Position(x-1, y-1),
-            Position(x-1, y),
-            Position(x-1, y+1),
-            Position(x, y-1),
-            Position(x, y+1),
-            Position(x+1, y-1),
-            Position(x+1, y),
-            Position(x+1, y+1)
-    };
 
-    for (int i = 0; i < sizeof(surroundingPositions) / sizeof(Position); i++) {
-        if (position->x == surroundingPositions[i].x && position->y == surroundingPositions[i].y){
-            return true;
+    bool ChessPiece :: queenCheck(Position * position){
+        int x = (this->position).x;
+        int y = (this->position).y;
+        if( 0 > x || x > 8 || 0 > y || y > 8){
+            return false;
         }
-    }
-    return false;
-}
-
-bool ChessPiece :: queenCheck(Position * position){
-    int x = (this->position).x;
-    int y = (this->position).y;
-    if( 0 > x || x > 8 || 0 > y || y > 8){
+        std::vector<Position> possiblePositions;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if(i == (this->position).x || j == (this->position).y){
+                    possiblePositions.push_back(Position(i,j));
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            switch (i){
+                case 0 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j >= 0 && k >= 0){
+                        possiblePositions.push_back(Position(j--,k--));
+                    }
+                }
+                    break;
+                case 1 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j >= 0 && k < COLUMNS){
+                        possiblePositions.push_back(Position(j--,k++));
+                    }
+                }
+                    break;
+                case 2 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j < ROWS && k >= 0){
+                        possiblePositions.push_back(Position(j++,k--));
+                    }
+                }
+                    break;
+                case 3 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j < ROWS && k < COLUMNS){
+                        possiblePositions.push_back(Position(j++,k++));
+                    }
+                }
+                    break;
+            }
+        }
+        for (auto possiblePosition: possiblePositions) {
+            if(possiblePosition.x == position->x && possiblePosition.y == position->y){
+                return true;
+            }
+        }
         return false;
     }
-    std::vector<Position> possiblePositions;
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLUMNS; j++) {
-            if(i == (this->position).x || j == (this->position).y){
-                possiblePositions.push_back(Position(i,j));
-            }
-        }
-    }
-    for (int i = 0; i < 4; i++) {
-        switch (i){
-            case 0 :
-            {
-                int j = x;
-                int k = y;
-                while(j >= 0 && k >= 0){
-                    possiblePositions.push_back(Position(j--,k--));
-                }
-            }
-                break;
-            case 1 :
-            {
-                int j = x;
-                int k = y;
-                while(j >= 0 && k < COLUMNS){
-                    possiblePositions.push_back(Position(j--,k++));
-                }
-            }
-                break;
-            case 2 :
-            {
-                int j = x;
-                int k = y;
-                while(j < ROWS && k >= 0){
-                    possiblePositions.push_back(Position(j++,k--));
-                }
-            }
-                break;
-            case 3 :
-            {
-                int j = x;
-                int k = y;
-                while(j < ROWS && k < COLUMNS){
-                    possiblePositions.push_back(Position(j++,k++));
-                }
-            }
-                break;
-        }
-    }
-    for (auto possiblePosition: possiblePositions) {
-        if(possiblePosition.x == position->x && possiblePosition.y == position->y){
-            return true;
-        }
-    }
-    return false;
-}
 
-bool ChessPiece :: pawnCheck(Position * position){
-    int x = (this->position).x;
-    int y = (this->position).y;
-    if( 0 > x || x > 8 || 0 > y || y > 8){
+    bool ChessPiece :: pawnCheck(Position * position){
+        int x = (this->position).x;
+        int y = (this->position).y;
+        if( 0 > x || x > 8 || 0 > y || y > 8){
+            return false;
+        }
+        if(isWhite){
+            if((this->position.x + 1 == position->x) || (this->position.x == 1 && position->x == 3) && (this->position.y == position->y)){
+                return true;
+            }
+            else return false;
+        }
+        else{
+            if((this->position.x - 1 == position->x) || (this->position.x == 6 && position->x == 4) && (this->position.y == position->y)){
+                return true;
+            }
+            else return false;
+        }
+    }
+
+    bool ChessPiece :: rookCheck(Position * position){
+        int x = (this->position).x;
+        int y = (this->position).y;
+        if( 0 > x || x > 8 || 0 > y || y > 8){
+            return false;
+        }
+        std::vector<Position> possiblePositions;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if(i == (this->position).x || j == (this->position).y){
+                    possiblePositions.push_back(Position(i,j));
+                }
+            }
+        }
+        for (auto possiblePosition : possiblePositions) {
+            printf("(%d, %d), ", possiblePosition.x, possiblePosition.y);
+        }
+        printf("\n");
+        for (auto possiblePosition: possiblePositions) {
+            if(possiblePosition.x == position->x && possiblePosition.y == position->y){
+                return true;
+            }
+        }
         return false;
     }
-    if(isWhite){
-        if((this->position.x + 1 == position->x) || (this->position.x == 1 && position->x == 3) && (this->position.y == position->y)){
-            return true;
-        }
-        else return false;
-    }
-    else{
-        if((this->position.x - 1 == position->x) || (this->position.x == 6 && position->x == 4) && (this->position.y == position->y)){
-            return true;
-        }
-        else return false;
-    }
-}
 
-bool ChessPiece :: rookCheck(Position * position){
-    int x = (this->position).x;
-    int y = (this->position).y;
-    if( 0 > x || x > 8 || 0 > y || y > 8){
+    bool ChessPiece :: knightCheck(Position * position){
+        int x = (this->position).x;
+        int y = (this->position).y;
+        if( 0 > x || x > 8 || 0 > y || y > 8){
+            return false;
+        }
+        Position possiblePositions[] = {
+                Position(x-2, y-1),
+                Position(x-2, y+1),
+                Position(x+2, y-1),
+                Position(x+2, y+1),
+                Position(x-1, y-2),
+                Position(x-1, y+2),
+                Position(x+1, y-2),
+                Position(x+1, y+2)
+        };
+        for (auto possiblePosition: possiblePositions) {
+            if(possiblePosition.x == position->x && possiblePosition.y == position->y){
+                return true;
+            }
+        }
         return false;
     }
-    std::vector<Position> possiblePositions;
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLUMNS; j++) {
-            if(i == (this->position).x || j == (this->position).y){
-                possiblePositions.push_back(Position(i,j));
+
+    bool ChessPiece :: bishopCheck(Position * position){
+        int x = (this->position).x;
+        int y = (this->position).y;
+        if( 0 > x || x > 8 || 0 > y || y > 8){
+            return false;
+        }
+        std::vector<Position> possiblePositions;
+        for (int i = 0; i < 4; i++) {
+            switch (i){
+                case 0 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j >= 0 && k >= 0){
+                        possiblePositions.push_back(Position(j--,k--));
+                    }
+                }
+                    break;
+                case 1 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j >= 0 && k < COLUMNS){
+                        possiblePositions.push_back(Position(j--,k++));
+                    }
+                }
+                    break;
+                case 2 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j < ROWS && k >= 0){
+                        possiblePositions.push_back(Position(j++,k--));
+                    }
+                }
+                    break;
+                case 3 :
+                {
+                    int j = x;
+                    int k = y;
+                    while(j < ROWS && k < COLUMNS){
+                        possiblePositions.push_back(Position(j++,k++));
+                    }
+                }
+                    break;
             }
         }
-    }
-    for (auto possiblePosition : possiblePositions) {
-        printf("(%d, %d), ", possiblePosition.x, possiblePosition.y);
-    }
-    printf("\n");
-    for (auto possiblePosition: possiblePositions) {
-        if(possiblePosition.x == position->x && possiblePosition.y == position->y){
-            return true;
+        for (auto possiblePosition: possiblePositions) {
+            if(possiblePosition.x == position->x && possiblePosition.y == position->y){
+                return true;
+            }
         }
-    }
-    return false;
-}
-
-bool ChessPiece :: knightCheck(Position * position){
-    int x = (this->position).x;
-    int y = (this->position).y;
-    if( 0 > x || x > 8 || 0 > y || y > 8){
         return false;
     }
-    Position possiblePositions[] = {
-            Position(x-2, y-1),
-            Position(x-2, y+1),
-            Position(x+2, y-1),
-            Position(x+2, y+1),
-            Position(x-1, y-2),
-            Position(x-1, y+2),
-            Position(x+1, y-2),
-            Position(x+1, y+2)
-    };
-    for (auto possiblePosition: possiblePositions) {
-        if(possiblePosition.x == position->x && possiblePosition.y == position->y){
-            return true;
-        }
-    }
-    return false;
-}
-
-bool ChessPiece :: bishopCheck(Position * position){
-    int x = (this->position).x;
-    int y = (this->position).y;
-    if( 0 > x || x > 8 || 0 > y || y > 8){
-        return false;
-    }
-    std::vector<Position> possiblePositions;
-    for (int i = 0; i < 4; i++) {
-        switch (i){
-            case 0 :
-            {
-                int j = x;
-                int k = y;
-                while(j >= 0 && k >= 0){
-                    possiblePositions.push_back(Position(j--,k--));
-                }
-            }
-                break;
-            case 1 :
-            {
-                int j = x;
-                int k = y;
-                while(j >= 0 && k < COLUMNS){
-                    possiblePositions.push_back(Position(j--,k++));
-                }
-            }
-                break;
-            case 2 :
-            {
-                int j = x;
-                int k = y;
-                while(j < ROWS && k >= 0){
-                    possiblePositions.push_back(Position(j++,k--));
-                }
-            }
-                break;
-            case 3 :
-            {
-                int j = x;
-                int k = y;
-                while(j < ROWS && k < COLUMNS){
-                    possiblePositions.push_back(Position(j++,k++));
-                }
-            }
-                break;
-        }
-    }
-    for (auto possiblePosition: possiblePositions) {
-        if(possiblePosition.x == position->x && possiblePosition.y == position->y){
-            return true;
-        }
-    }
-    return false;
 }
--- raylib_template/src/chessPiece.h
+++ raylib_template/src/chessPiece.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include "raylib.h"
-#include "chessBoard.h"
+#include "ChessBoard.h"
 #include <stdio.h>
 #include <cmath>
 #include <string>
@@ -13,97 +13,100 @@
 #define ROWS 8
 #define COLUMNS 8
 
-#ifndef CHESS_CHESSPIECE_H
-#define CHESS_CHESSPIECE_H
+#ifndef CHESS_CPP_CHESSPIECE_H
+#define CHESS_CPP_CHESSPIECE_H
 
-enum PieceType {
-    KING,
-    QUEEN,
-    KNIGHT,
-    BISHOP,
-    ROOK,
-    PAWN
-};
+namespace ChessPiece_N{
+    enum PieceType {
+        KING,
+        QUEEN,
+        KNIGHT,
+        BISHOP,
+        ROOK,
+        PAWN
+    };
 
-class Position{
-public:
-    int x;
-    int y;
+    class Position{
+    public:
+        int x;
+        int y;
 
-    Position(){
+        Position(){
 
-    }
-
-    Position(int X, int Y){
-        x = X;
-        y = Y;
-    }
-};
-
-class ChessPiece {
-protected:
-    bool selected = false;
-public:
-    Image pieceImage;
-    Rectangle pieceRectangle;
-    Position position;
-    PieceType type;
-    bool isWhite;
-    bool isCaptured = false;
-    ChessPiece(){}
-
-    bool isSelected() const {
-        return selected;
-    }
-
-    void setSelected(bool value) {
-        selected = value;
-    }
-
-    void setPosition(int x, int y) {
-        this->position.x = x;
-        this->position.y = y;
-    }
-
-    virtual bool isValidMove(Position * position) = 0;
-
-    void MoveToPosition(Position * position){
-        printf("Current Position: %d, %d\n", (this->position).x, (this->position).y);
-        printf("Target Position: %d, %d\n", position->x, position->y);
-        setPosition(position->x, position->y);
-    }
-
-    std::string getType();
-
-    bool kingCheck(Position * position);
-
-    bool queenCheck(Position * position);
-
-    bool rookCheck(Position * position);
-
-    bool pawnCheck(Position * position);
-
-    bool knightCheck(Position * position);
-
-    bool bishopCheck(Position * position);
-
-    bool validityCheck(Position * position){
-        switch (this->type)
-        {
-            case KING:
-                return kingCheck(position);
-            case QUEEN:
-                return queenCheck(position);
-            case ROOK:
-                return rookCheck(position);
-            case KNIGHT:
-                return knightCheck(position);
-            case BISHOP:
-                return bishopCheck(position);
-            case PAWN:
-                return pawnCheck(position);
         }
-    }
-};
+
+        Position(int X, int Y){
+            x = X;
+            y = Y;
+        }
+    };
+
+    class ChessPiece {
+    protected:
+        bool selected = false;
+    public:
+        Image pieceImage;
+        Rectangle pieceRectangle;
+        Position position;
+        PieceType type;
+        bool isWhite;
+        bool isCaptured = false;
+        ChessPiece(){}
+
+        bool isSelected() const {
+            return selected;
+        }
+
+        void setSelected(bool value) {
+            selected = value;
+        }
+
+        void setPosition(int x, int y) {
+            this->position.x = x;
+            this->position.y = y;
+        }
+
+        virtual bool isValidMove(Position * position) = 0;
+
+        void MoveToPosition(Position * position){
+            printf("Current Position: %d, %d\n", (this->position).x, (this->position).y);
+            printf("Target Position: %d, %d\n", position->x, position->y);
+            setPosition(position->x, position->y);
+        }
+
+        std::string getType();
+
+        bool kingCheck(Position * position);
+
+        bool queenCheck(Position * position);
+
+        bool rookCheck(Position * position);
+
+        bool pawnCheck(Position * position);
+
+        bool knightCheck(Position * position);
+
+        bool bishopCheck(Position * position);
+
+        bool validityCheck(Position * position){
+            switch (this->type)
+            {
+                case KING:
+                    return kingCheck(position);
+                case QUEEN:
+                    return queenCheck(position);
+                case ROOK:
+                    return rookCheck(position);
+                case KNIGHT:
+                    return knightCheck(position);
+                case BISHOP:
+                    return bishopCheck(position);
+                case PAWN:
+                    return pawnCheck(position);
+                default: return false;
+            }
+        }
+    };
+}
 
 #endif //CHESS_CPP_CHESSPIECE_H
